b70b1784feaedb89128017019d83bcb8c9ac448b
AlejandroVG4 <Vanegasgarcia1104@gmail.com>
2024-10-01 00:30:03 -0500

    Agregando vista usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from .models import Productos
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
+
+@csrf_exempt
+def index(request, id=None):
+    
+    if request.method == 'GET':
+        pass
+    if request.method == 'POST':
+        pass
+    if request.method == 'DELETE':
+        pass
+    if request.method == 'PUT':
+        pass
+    if request.method == 'PATCH':
+        pass
